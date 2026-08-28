95918eb1eefeaffec061d74dc85398be3277ac06
CHETTI GANESH <152371084+chettiganesh@users.noreply.github.com>
2026-08-28 23:32:48 +0530

    Time: 13 ms (10.61%), Space: 24.4 MB (7.49%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,10 +2,22 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
        int n = nums.size();
-       int ones=0;
-       for(int i=0;i<n;i++){
-        ones^=nums[i];
-       } 
-       return ones;
+    //    int ones=0;
+    //    for(int i=0;i<n;i++){
+    //     ones^=nums[i];
+    //    } 
+    //    return ones;
+
+    map<int,int> mp;
+    for(int i=0;i<n;i++){
+        mp[nums[i]]++;
+    }
+    for(auto elements:mp){
+        if(elements.second==1){
+            return elements.first;
+        }
+       
+    }
+     return -1;
     }
 };

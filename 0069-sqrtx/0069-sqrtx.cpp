b3103341a5140db0f167066531067093a9d214d5
CHETTI GANESH <152371084+chettiganesh@users.noreply.github.com>
2026-09-03 23:30:06 +0530

    Time: 1 ms (23.59%), Space: 8.6 MB (14.4%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        long l=1,r=x/2;
+        while(l<=r){
+            long mid=l+(r-l)/2;
+            long sq=mid*mid;
+            if(sq==x) return    mid;
+            if(sq<x) l=mid+1;
+            else r=mid-1;
+        }
+        return r;
+    }
+};
